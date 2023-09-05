4f3ba49f87a875f5bda0588abfb27f17d6da4940
Carlos Antonio Tarqui Guillen <143270621+nntslip@users.noreply.github.com>
2023-09-05 19:57:39 -0400

    Update LiskovSubtitution.ts

--- Tarea 2/LiskovSubtitution.ts
+++ Tarea 2/LiskovSubtitution.ts
@@ -1,60 +1,97 @@
-class Pajaro {
-    volar() {
-        console.log('El pajaro vuela en el cielo');
-    }
-}
-
-class Pinguino extends Pajaro {
-    volar() {
-        throw new Error('Los pinguinos no pueden volar');
-    }
-}
-/////////
-class Rectángulo {
-
-    alto: number;
-    ancho: number;
-
-    constructor(alto: number, ancho: number) {
-        this.alto = alto;
-        this.ancho = ancho;
-    }
-
-    setAlto(value: number) {
-        this.alto = value;
-    }
-
-    setAncho(value: number) {
-        this.ancho = value;
-    }
-
-    area(): number {
-        return this.alto * this.ancho;
-    }
-
-}
-
-class Cuadrado extends Rectángulo {
-
-    constructor(lado: number) {
-        super(lado, lado);
-    }
-
-    setAlto(value: number) {
-        if (value !== this.ancho) {
-            throw new Error("Los lados de un cuadrado deben ser iguales");
-        }
-
-        super.setAlto(value);
-        this.ancho = value;
-    }
-
-    setAncho(value: number) {
-        if (value !== this.alto) {
-            throw new Error("Los lados de un cuadrado deben ser iguales");
-        }
-
-        super.setAncho(value);
-        this.alto = value;
-    }
-}
+//example 1 
+ // class Pajaro{ 
+ //     volar(){ 
+ //         console.log("El pájaro vuela en el cielo"); 
+ //     } 
+ // } 
+  
+ // class Pinguino extends Pajaro{ 
+ //     volar(){ 
+ //         throw new Error("Los pinguinos no pueden volar"); 
+ //     } 
+ // } 
+  
+ /////solucion 
+  
+ class Aves{ 
+     nombre:string; 
+     constructor(nombre:string){ 
+         this.nombre=nombre; 
+     } 
+  
+     volar(){ 
+         console.log("El pájaro vuela en el cielo"); 
+     } 
+  
+     novolar(){ 
+         throw new Error("Los pinguinos no pueden volar"); 
+     } 
+ } 
+  
+ class Pajaro extends Aves{ 
+     constructor(nombre:string){ 
+         super(nombre); 
+     } 
+     volar(): string { 
+         return ""; 
+     } 
+ } 
+  
+ class Pinguino extends Aves{ 
+     constructor(nombre:string){ 
+         super(nombre); 
+     } 
+     novolar(): string { 
+         return ""; 
+     } 
+ } 
+  
+  
+ //example 2 
+ class Rectangulo{ 
+     alto: number; 
+     ancho: number; 
+  
+     constructor(alto: number, ancho: number){ 
+         this.alto = alto; 
+         this.ancho = ancho; 
+     } 
+  
+     setAlto(value: number){ 
+         this.alto = value; 
+     } 
+  
+     setAncho(value: number){ 
+         this.ancho = value; 
+     } 
+  
+     area(): number{ 
+         return this.alto = this.ancho; 
+     } 
+ } 
+  
+ /// 
+ class Cuadrado extends Rectangulo { 
+  
+     constructor(lado: number) { 
+         super(lado, lado); 
+     } 
+  
+     setAlto(value: number) { 
+         if (value !== this.ancho) { 
+             throw new Error("Los lados del cuadrado deben de ser iguales"); 
+         } 
+  
+         super.setAlto(value); 
+         this.ancho = value; 
+     } 
+  
+     setAncho(value: number) { 
+         if (value !== this.alto) { 
+             throw new Error("Los lados del cuadrado deben de ser iguales"); 
+         } 
+  
+         super.setAncho(value); 
+         this.alto = value; 
+     } 
+     }
