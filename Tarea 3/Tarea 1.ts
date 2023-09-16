412e7831766c0679c3a11c7d70fdcce9054710fd
Carlos Antonio Tarqui Guillen <143270621+nntslip@users.noreply.github.com>
2023-09-16 09:19:41 -0400

    Update Tarea 1.ts

--- Tarea 3/Tarea 1.ts
+++ Tarea 3/Tarea 1.ts
@@ -1,3 +1,10 @@
+/*Tarea 1 /Problema:
+Supongamos que queremos expandir nuestro sistema para gestionar multas para aquellos usuarios que no devuelvan los libros a tiempo. Cada libro que se pase de la fecha de devolución generará una multa diaria.
+Requisitos:
+1.	Cada libro tiene un tiempo de préstamo estándar de 7 días.
+2.	Una vez que un libro se presta, se debe registrar la fecha de préstamo.
+3.	Cuando un libro es devuelto, el sistema debe calcular si se ha pasado del tiempo estándar y, si es así, determinar cuántos días se ha pasado.
+4.	Por cada día de retraso, se debe generar una multa de $1.*/
 class Libro {
     constructor(public titulo: string, public autor: string, public estaCargado: boolean = false, public estaDisponible: string, public fechaPrestamo?: Date) { }
 }
