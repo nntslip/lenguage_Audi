7cd722991b3e3b3ccd23f4ed18d63fed0ca9c177
Carlos Antonio Tarqui Guillen <143270621+nntslip@users.noreply.github.com>
2023-09-16 09:30:05 -0400

    Update Tarea 2.ts

--- Tarea 3/Tarea 2.ts
+++ Tarea 3/Tarea 2.ts
@@ -1,3 +1,16 @@
+/*Tarea 2
+Expandamos aún más nuestro sistema para incorporar una funcionalidad de membresía y categorización de libros.
+Requisitos:
+1.	Membresías de usuario:
+•	Tres tipos de membresías: Básico, Premium y Platino.
+•	Básico: Puede pedir prestado un máximo de 2 libros a la vez.
+•	Premium: Puede pedir prestado un máximo de 5 libros a la vez.
+•	Platino: Puede pedir prestado un número ilimitado de libros.
+2.	Categorización de libros:
+•	Los libros se categorizan en Ficción, No Ficción, y Referencia.
+•	Los libros de Referencia no se pueden prestar. Son solo para leer en la biblioteca.
+3.	Historial de préstamos:
+•	Mantener un registro de todos los préstamos que ha hecho un usuario, independientemente de si el libro ha sido devuelto o no.*/
 interface IMembresia {
     puedeTomarPrestado(cantidadLibros: number): boolean;
 }
